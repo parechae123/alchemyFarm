e0cb92c5d0dc30791fbf7566c92ea3320342fa70
parechae123 <parechae@gmail.com>
2023-03-30 01:34:03 +0900

    미리보기 rotation.w 반영

--- LastWinterVacation/Assets/01.Scripts/CameraRaycaster.cs
+++ LastWinterVacation/Assets/01.Scripts/CameraRaycaster.cs
@@ -21,8 +21,8 @@ public class CameraRaycaster : MonoBehaviour
             Debug.DrawRay(transform.position, transform.forward * 10, Color.red);
             rayPos = groundHit.point;
             cube.transform.position = rayPos;
-            cube.transform.rotation = new Quaternion(0, transform.rotation.y, 0,1);
-
+            cube.transform.rotation = new Quaternion(0, transform.rotation.y, 0, transform.rotation.w);
+            Debug.Log(cube.transform.rotation);
         }
         else
         {

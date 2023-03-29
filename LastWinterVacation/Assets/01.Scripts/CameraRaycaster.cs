403f93e200e3f08deaa8b35cf394f8764735ecc2
Heoyoon <parechae@gmail.com>
2023-03-29 15:02:13 +0900

    카메라 레이캐스트 적용 및 큐브 미리보기

---
+++ LastWinterVacation/Assets/01.Scripts/CameraRaycaster.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRaycaster : MonoBehaviour
+{
+    public LayerMask isGround;
+    private RaycastHit groundHit;
+    public Vector3 rayPos;
+    private GameObject cube;
+    private void Awake()
+    {
+        cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+    }
+    void Update()
+    {
+        
+        Ray ray = new Ray(transform.position, transform.forward * 10);
+        if (Physics.Raycast(ray, out groundHit, 10, isGround))
+        {
+            Debug.DrawRay(transform.position, transform.forward * 10, Color.red);
+            rayPos = groundHit.point;
+            cube.transform.position = rayPos;
+            cube.transform.rotation = new Quaternion(0, transform.rotation.y, 0,1);
+
+        }
+        else
+        {
+            rayPos = new Vector3(0,0,0);
+        }
+    }
+}
